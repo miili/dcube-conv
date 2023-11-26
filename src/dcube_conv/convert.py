4a7623a84c4a1a055e34cabe6edb690acb877629
Marius Isken <mi@gfz-potsdam.de>
2023-11-27 00:05:08 +0100

    functional prototype

--- src/dcube_conv/convert.py
+++ src/dcube_conv/convert.py
@@ -1,12 +1,21 @@
 import asyncio
 import logging
+from datetime import datetime, timedelta
 from pathlib import Path
 from typing import TYPE_CHECKING, AsyncIterator, Iterator
 
-from pydantic import BaseModel, DirectoryPath, PrivateAttr
+from pydantic import (
+    BaseModel,
+    DirectoryPath,
+    Field,
+    PositiveInt,
+    PrivateAttr,
+    computed_field,
+)
 from pyrocko.io import datacube
 
-from dcube_conv.model import DatacubeTraces
+from dcube_conv.model import DatacubeTraces, RecordLength, SteimCompression
+from dcube_conv.utils import human_readable_bytes
 
 if TYPE_CHECKING:
     pass
@@ -14,30 +23,55 @@ if TYPE_CHECKING:
 logger = logging.getLogger(__name__)
 
 
-class InputStats(BaseModel):
+class LoadingStats(BaseModel):
     n_files: int = 0
-    size_bytes: int = 0
+    size_bytes_total: int = 0
+    bytes_loaded: int = 0
+    time_started: datetime = Field(default_factory=datetime.now)
+
+    @computed_field
+    def time_elapsed(self) -> timedelta:
+        return datetime.now() - self.time_started
+
+    @computed_field
+    @property
+    def loading_rate(self) -> float:
+        return self.bytes_loaded / self.time_elapsed.total_seconds()
+
+    @computed_field
+    @property
+    def time_remaining(self) -> timedelta:
+        if self.loading_rate == 0:
+            return timedelta(seconds=0)
+        return timedelta(
+            seconds=(self.size_bytes_total - self.bytes_loaded) / self.loading_rate
+        )
+
+    @computed_field
+    @property
+    def percent(self) -> float:
+        return self.bytes_loaded / self.size_bytes_total * 100
 
 
-class InputDataCube(BaseModel):
+class LoaderDataCube(BaseModel):
     path: DirectoryPath = DirectoryPath(".")
 
-    _stats: InputStats = PrivateAttr(default_factory=InputStats)
+    _stats: LoadingStats = PrivateAttr(default_factory=LoadingStats)
     _files: list[Path] = PrivateAttr(default_factory=list)
 
     def _iter_datacube_files(self) -> Iterator[Path]:
-        for dirpath, _, filenames in self.path.walk():
-            for filename in filenames:
-                file = dirpath / filename
-                with file.open() as f:
-                    if datacube.detect(f.read(512)):
-                        yield file
+        logger.info("Scanning %s", self.path)
+        for file in self.path.rglob("*.*"):
+            with file.open("rb") as f:
+                if datacube.detect(f.read(512)):
+                    yield file
 
     def scan(self) -> None:
         for file in self._iter_datacube_files():
             self._stats.n_files += 1
-            self._stats.size_bytes += file.stat().st_size
+            self._stats.size_bytes_total += file.stat().st_size
             self._files.append(file)
+        print(self._stats)
 
     def __iter__(self) -> Iterator[Path]:
         return iter(self._files)
@@ -48,6 +82,7 @@ class InputDataCube(BaseModel):
 
         async def worker(file: Path) -> None:
             await queue.put(await asyncio.to_thread(DatacubeTraces.from_file, file))
+            self._stats.bytes_loaded += file.stat().st_size
 
         async def fetch(n_files: int = 1) -> None:
             logger.info("Fetching %d files", n_files)
@@ -59,35 +94,53 @@ class InputDataCube(BaseModel):
                         break
                     tg.create_task(worker(file))
 
-        while True:
-            asyncio.create_task(fetch(queue.maxsize - queue.qsize()))
-            datacube = await queue.get()
-            if datacube is None:
-                break
-            yield datacube
-            queue.task_done()
+        async with asyncio.TaskGroup() as tg:
+            tg.create_task(fetch(queue.maxsize))
+
+            while True:
+                tg.create_task(fetch(1))
+                datacube = await queue.get()
+                if datacube is None:
+                    break
+                yield datacube
+                queue.task_done()
+                print(
+                    human_readable_bytes(self._stats.loading_rate),
+                    self._stats.time_remaining,
+                )
 
 
 class Converter(BaseModel):
-    input: InputDataCube = InputDataCube()
+    input: LoaderDataCube = LoaderDataCube()
     output_path: DirectoryPath = DirectoryPath(".")
+    output_template: str = "%(network)s/%(station)s/%(location)s/%(channel)s.mseed"
+
+    record_length: RecordLength = 4096
+    steim_compression: SteimCompression = 1
+
+    write_threads: PositiveInt = 20
 
     async def _async_save(self, datacube: AsyncIterator[DatacubeTraces]) -> None:
+        limit = asyncio.Semaphore(self.write_threads)
+
         async def worker(datacube: DatacubeTraces) -> None:
-            await asyncio.to_thread(datacube.save, self.output_path)
+            async with limit:
+                await asyncio.to_thread(
+                    datacube.save,
+                    self.output_path / self.output_template,
+                    record_length=self.record_length,
+                    steim=self.steim_compression,
+                )
 
-        async def save() -> None:
-            async with asyncio.TaskGroup() as tg:
-                async for dc in datacube:
-                    tg.create_task(worker(dc))
-
-        await save()
+        async with asyncio.TaskGroup() as tg:
+            async for dc in datacube:
+                tg.create_task(worker(dc))
 
     async def convert(self) -> None:
         self.input.scan()
         logger.info(
             "Found %d datacube files (%.2f GB)",
             self.input._stats.n_files,
-            self.input._stats.size_bytes / 1e9,
+            self.input._stats.size_bytes_total / 1e9,
         )
         await self._async_save(self.input.iter_datacube())
--- src/dcube_conv/model/__init__.py
+++ src/dcube_conv/model/__init__.py
@@ -1,31 +1,37 @@
 import logging
+from dataclasses import dataclass
 from pathlib import Path
-from typing import Literal, Self
+from typing import Any, Literal, Self
 
-from pydantic.dataclasses import dataclass
 from pyrocko.io import datacube, save
 from pyrocko.trace import Trace
 
 logger = logging.getLogger(__name__)
 
 SteimCompression = Literal[None, 1, 2]
+RecordLength = Literal[512, 1024, 2048, 4096, 8192]
 
 
 @dataclass
 class DatacubeTraces:
     path: Path
     traces: list[Trace]
+    gps_tags: Any
 
     @classmethod
     def from_file(cls, file: Path) -> Self:
         logger.debug("Loading %s", file)
-        return cls(path=file, traces=[tr for tr in datacube.iload(str(file))])
+        data = [
+            (tr, gps_tags)
+            for (tr, gps_tags) in datacube.iload(str(file), yield_gps_tags=True)
+        ]
+        return cls(path=file, traces=[tr for (tr, _) in data], gps_tags=data[0][1])
 
     def save(
         self,
         output_path: Path,
-        record_length: int = 4096,
+        record_length: RecordLength = 4096,
         steim: SteimCompression = 1,
     ) -> None:
         logger.info("Saving miniSeed %s", output_path)
-        save(str(output_path), self.traces)
+        save(self.traces, str(output_path), record_length=record_length, steim=steim)
